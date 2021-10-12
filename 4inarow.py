844abaa65886f0216447b51442ae4e416db64644
Teije Langelaan <t.j.langelaan@outlook.com>
2021-10-12 14:06:22 +0200

    Graphics finished

--- 4inarow.py
+++ 4inarow.py
@@ -1,1 +1,19 @@
 #This is going to be my first python game :)
+
+from colorama import Fore, Back, Style
+
+playingboard = '''
+ /=================================\ 
+||___||___||___||___||___||___||___||
+||___||___||___||___||___||___||___||
+||___||___||___||___||___||___||___||
+||___||___||___||___||___||___||___||
+||___||___||___||___||___||___||___||
+||___||___||___||___||___||___||___||
+/\                                 /\ '''
+index = 262
+#playingboard update function
+playingboard = playingboard[:index] + '1' + playingboard[index + 1:]
+index = 257
+playingboard = playingboard[:index] + '0' + playingboard[index + 1:]
+print(playingboard)
