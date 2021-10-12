a2ce8b8644766d69715e31b59e20bbfb5732bc0c
Teije Langelaan <t.j.langelaan@outlook.com>
2021-10-12 20:55:25 +0200

    updated visual system

--- 4inarow.py
+++ 4inarow.py
@@ -12,24 +12,23 @@ playingboard = '''
 /\ 1    2    3    4    5    6    7 /\ '''
 
 game = 'started'
-counter = [0, 0, 0, 0, 0 ,0, 0]
+counter = {'1': 0, '2': 0, '3': 0, '4': 0, '6': 0, '7': 0}
 row1 = {'1': 232, '2': 237, '3': 242, '4': 247, '6': 252, '7': 257}
-print(sum(counter))
+print(sum(counter.values()))
 
 
 print(playingboard)
 while game != 'won':
-    if sum(counter) % 2 != 0:
+    if sum(counter.values()) % 2 == 0:
         player = '0'
     else:
         player = '1'
 
     choice = input('Choose column... ')
-    if choice == '1':
-        index = 232 - (38 * counter[0])
-        playingboard = playingboard[:index] + player + playingboard[index + 1:]
-        counter[0] += 1
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print(playingboard)
-    elif choice == '2':
-        break
+
+    index = row1[choice] - (38 * counter[choice])
+    playingboard = playingboard[:index] + player + playingboard[index + 1:]
+    counter[choice] += 1
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(playingboard)
+
